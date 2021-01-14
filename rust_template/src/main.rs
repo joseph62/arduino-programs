0dc0698dba2bd89e527e055b6a15d1de34f63ea0
Sean Joseph <theseanjoseph@gmail.com>
2021-01-13 22:32:47 -0500

    Updated rust template to include lines that prep pin interaction

--- rust_template/src/main.rs
+++ rust_template/src/main.rs
@@ -3,11 +3,13 @@
 
 extern crate panic_halt;
 use arduino_uno::prelude::*;
+use arduino_uno::{Peripherals, Pins};
 
 #[arduino_uno::entry]
 fn main() -> ! {
+    let dp = Peripherals::take().unwrap();
+    let mut pins = Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
 
     loop {
-
     }
 }
